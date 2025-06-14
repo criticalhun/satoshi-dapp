d51ad6e7ab1f83a03019b30250209c39490837c5
criticalhun <criticalhun@proton.me>
2025-06-14 20:32:13 +0200

    fix: dark mode működés, dark wrapper, config és index.js frissítés

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
-  theme: {
-    extend: {},
-  },
+  darkMode: "class", // <<< MUSZÁJ
+  theme: { extend: {} },
   plugins: [],
 };
