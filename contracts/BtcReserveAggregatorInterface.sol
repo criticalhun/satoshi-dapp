599ff4f8858b2d996da428e366740c432e8f9a85
criticalhun <criticalhun@proton.me>
2025-06-23 20:41:05 +0200

    chore: update SPDX/license comment in all contracts

--- contracts/BtcReserveAggregatorInterface.sol
+++ contracts/BtcReserveAggregatorInterface.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: OTHER
 pragma solidity ^0.8.20;
 
 interface AggregatorV3Interface {
--- contracts/ChainlinkProofOfReserveFeed.sol
+++ contracts/ChainlinkProofOfReserveFeed.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: OTHER
 pragma solidity ^0.8.20;
 
 import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
